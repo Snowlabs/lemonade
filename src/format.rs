32bfd41d53158ceacf4d4bf983996b1bdcfac4b0
Sheheryar Parvaz <skipper308@hotmail.ca>
2017-09-24 17:02:11 -0400

    Add basic lemonbar parser

--- src/format.rs
+++ src/format.rs
@@ -6,8 +6,54 @@ pub struct Color {
 }
 
 impl Color {
-    pub fn new(r: f64, g: f64, b: f64, a: f64) -> Color {
+    pub fn new(r: f64, g: f64, b: f64, a: f64) -> Self {
         Color { r, g, b, a, }
+    }
+
+    // Takes either: #rrggbb, #aarrggbb, #rbg, #argb
+    // The '#' in front is not necessary
+    pub fn from_hex(hex: &str) -> Result<Color, &'static str>{
+        let mut s = String::from(hex);
+        let mut c = [1.0, 0.0, 0.0, 0.0]; // argb
+
+        // Remove the prepended #
+        if s.get(0..1) == Some("#") {
+            s.remove(0);
+        }
+
+        // Make sure the length is ok, and set the multiplier
+        // used when iterating.
+        let mut m: usize;
+        let mut b: usize;
+        match s.len() {
+            3 => { m = 1; b = 1 }
+            4 => { m = 1; b = 0 }
+            6 => { m = 2; b = 1 }
+            8 => { m = 2; b = 0 }
+            _ => return Err("Provided string as invalid length"),
+        }
+
+        let mut it = s.chars().peekable();
+        let mut n = 0;
+        while it.peek().is_some() {
+            let h: String = it.by_ref().take(m).collect();
+            let v = i32::from_str_radix(&h, 16).unwrap();
+            c[b + n] = v as f64 / (16.0_f64.powi(m as i32) - 1.0);
+            n += 1;
+        }
+
+        Ok(Color {
+            a: c[0],
+            r: c[1],
+            g: c[2],
+            b: c[3],
+        })
+    }
+}
+
+impl Clone for Color {
+    fn clone(&self) -> Self {
+        Color::new(self.r, self.g, self.b, self.a)
     }
 }
 
@@ -20,6 +66,14 @@ pub struct Text {
 // Must be the last item of the vector!
 pub struct Filler {
     pub bg: Color,
+}
+
+impl Filler {
+    pub fn new(bg: Color) -> Self {
+        Filler {
+            bg
+        }
+    }
 }
 
 pub enum FormatItem {
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,138 @@
 extern crate lemonade;
+extern crate regex;
 
+use std::io;
+use regex::Regex;
 use lemonade::Bar;
 use lemonade::format::{FormatItem, Text, Filler, Color};
-//use std::io;
-use std::thread;
-use std::time;
 
 fn main() {
 
     let mut bar = Bar::new_xcb();
     bar.size(1920, 30);
 
-    //let mut fmt = Format::new();
+    let mut buf = String::new();
+    let mut lem = LemonParser::new();
 
-    let mut fmt: Vec<Vec<FormatItem>> = Vec::new();
-    fmt.push(Vec::new());
-    fmt.push(Vec::new());
-    fmt.push(Vec::new());
-
-    fmt[1].push(FormatItem::Text(Text {
-        bg: Color::new(1.0, 1.0, 1.0, 1.0),
-        fg: Color::new(0.2, 0.0, 0.0, 1.0),
-        text: String::from("some center text, "),
-    }));
-
-    fmt[1].push(FormatItem::Text(Text {
-        bg: Color::new(1.0, 1.0, 1.0, 1.0),
-        fg: Color::new(0.2, 0.0, 0.0, 0.7),
-        text: String::from("other center text"),
-    }));
-
-    fmt[1].push(FormatItem::Filler(Filler {
-        bg: Color::new(0.0, 0.0, 0.0, 1.0),
-    }));
-
-    fmt[2].push(FormatItem::Text(Text {
-        bg: Color::new(1.0, 8.0, 8.0, 1.0),
-        fg: Color::new(0.0, 0.2, 0.0, 0.5),
-        text: String::from("right text"),
-    }));
-
-    fmt[0].push(FormatItem::Text(Text {
-        bg: Color::new(1.0, 8.0, 8.0, 1.0),
-        fg: Color::new(0.0, 0.2, 0.0, 1.0),
-        text: String::from("left text"),
-    }));
-
-    fmt[0].push(FormatItem::Filler(Filler {
-        bg: Color::new(0.0, 0.0, 0.0, 1.0),
-    }));
-
-    //let mut buf = String::new();
-    let one_s = time::Duration::new(1, 0);
     loop {
-        //buf.clear();
+        buf.clear();
+        if let Err(_) = io::stdin().read_line(&mut buf) {
+            continue;
+        }
 
-        //if let Err(_) = io::stdin().read_line(&mut buf) {
-            //continue;
-        //}
+        let f = lem.parse(&buf);
 
-        bar.draw(&fmt);
-        thread::sleep(one_s);
+        bar.draw(&f);
+    }
+}
+
+pub struct LemonParser {
+    bg: Color,
+    fg: Color,
+    re: Regex,
+}
+
+impl LemonParser {
+    pub fn new() -> Self {
+        let re = Regex::new(concat!(
+            r"%\{(?P<type>",
+                "[lcr]", "|",
+                "[BF]", "#(?P<colo>(?:[[:xdigit:]]{3,4}){1,2})", //"|",
+            r")\}",
+            )
+        ).unwrap();
+
+        let bg = Color::new(1.0, 1.0, 1.0, 1.0);
+        let fg = Color::new(0.0, 0.0, 0.0, 1.0);
+
+        Self {
+            bg,
+            fg,
+            re,
+        }
+    }
+
+    pub fn parse(&mut self, fmt: &str) -> Vec<Vec<FormatItem>> {
+        // Temporary variables for computing string slices
+        let mut bpos: usize = 0;
+        let mut epos: usize = 0;
+
+        // Index of the current sub-vector being processed
+        let mut i: usize = 0;
+
+        // Return value
+        let mut v: Vec<Vec<FormatItem>> = Vec::with_capacity(3);
+        for _ in 0..3 { v.push(Vec::new()); } // Initialise vector
+
+        // Iterate through every formatting item
+        for mat in self.re.find_iter(fmt) {
+            let caps = self.re.captures(mat.as_str()).unwrap();
+            epos = mat.start();
+
+            self.pusht(&fmt[bpos..epos], &mut v[i]);
+
+            match *&caps["type"].chars().nth(0).unwrap() {
+                'l' => {
+                    self.pushf(&mut v[i]);
+                    i = 0;
+                }
+
+                'c' => {
+                    self.pushf(&mut v[i]);
+                    i = 1;
+                }
+
+                'r' => {
+                    self.pushf(&mut v[i]);
+                    i = 2;
+                }
+
+                'F' => {
+                    self.fg = Color::from_hex(&caps["colo"]).unwrap();
+                }
+
+                'B' => {
+                    self.bg = Color::from_hex(&caps["colo"]).unwrap();
+                }
+
+                _ => {}
+            }
+
+            bpos = mat.end();
+        }
+
+        if bpos > epos {
+            epos = fmt.len();
+        }
+
+        self.pusht(&fmt[bpos..epos], &mut v[i]);
+
+        return v;
+    }
+
+    fn pusht(&self, s: &str, v: &mut Vec<FormatItem>) {
+        if ! s.is_empty() {
+
+            if let Some(&FormatItem::Filler(_)) = v.last() {
+                v.pop();
+            }
+
+            v.push(FormatItem::Text(Text {
+                bg: self.bg.clone(),
+                fg: self.fg.clone(),
+                text: String::from(s),
+            }))
+        }
+    }
+
+    fn pushf(&self, v: &mut Vec<FormatItem>) {
+
+        if let Some(&FormatItem::Filler(_)) = v.last() {
+            v.pop();
+        }
+
+        v.push(FormatItem::Filler(Filler {
+            bg: self.bg.clone(),
+        }));
     }
 }
